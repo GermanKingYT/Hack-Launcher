529126295cd08b9794127400f6a850317fbec564
RaIN <razor13372k@gmail.com>
2017-01-16 23:56:44 +0100

    Updated Security

--- Upload Stuff/register.php
+++ Upload Stuff/register.php
@@ -4,28 +4,25 @@ $dbhost   		= "HOST";
 $dbname   		= "DB-NAME";
 $dbuser   		= "DB-USER";
 $dbpass   		= "DB-PASS";
+
 $encryptionPass	= "ENCRYPTION-PASSWORD";
 	
 if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['hwid'])) 
 {
 	$mysqli = new mysqli($dbhost, $dbuser , $dbpass, $dbname);
 	
-	$user       = mysqli_real_escape_string($mysqli, $_POST['username']);
-	$pass       = mysqli_real_escape_string($mysqli, $_POST['password']);
-	$hwid      	= mysqli_real_escape_string($mysqli, $_POST['hwid']);
+	$encrypted_user	= openssl_encrypt(mysqli_real_escape_string($mysqli,$_POST['username']),"AES-128-ECB",$encryptionPass);
+	$encrypted_pass	= openssl_encrypt(mysqli_real_escape_string($mysqli,$_POST['password']),"AES-128-ECB",$encryptionPass);
+	$encrypted_hwid	= openssl_encrypt(mysqli_real_escape_string($mysqli,$_POST['hwid']),"AES-128-ECB",$encryptionPass);
 	$active		= "false";
 	$try		= "0";
-
-	$encrypted_user	= openssl_encrypt($user,"AES-128-ECB",$encryptionPass);
-	$encrypted_pass	= openssl_encrypt($pass,"AES-128-ECB",$encryptionPass);
-	$encrypted_hwid	= openssl_encrypt($hwid ,"AES-128-ECB",$encryptionPass);
 	
 	if ($mysqli->connect_errno)  {
 		die("Connection Failed: " . $mysqli->connect_error);
 	}
 
-	$check_hwid			= "SELECT * FROM Users WHERE hwid='" . $encrypted_hwid . "'";
-	$check_username 	= "SELECT * FROM Users WHERE username='" . $encrypted_user . "'";
+	$check_hwid			= "SELECT * FROM Users WHERE hwid='".mysqli_real_escape_string($mysqli,$encrypted_hwid)."'";
+	$check_username 	= "SELECT * FROM Users WHERE username='".mysqli_real_escape_string($mysqli,$encrypted_user)."'";
 	$result_hwid		= $mysqli->query($check_hwid);
 	$result_username 	= $mysqli->query($check_username);
 	
@@ -37,7 +34,7 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['hwid
 		die("User already exists");
 	}
 	
-	$sql = "INSERT INTO `TestDB`.`Users` (`username`, `password`, `hwid`, `try`, `active`) VALUES ('" . $encrypted_user . "', '" . $encrypted_pass . "', '" . $encrypted_hwid . "', '" . $try . "', '" . $active . "')";
+	$sql = "INSERT INTO `TestDB`.`Users` (`username`, `password`, `hwid`, `try`, `active`) VALUES ('".mysqli_real_escape_string($mysqli,$encrypted_user)."', '".mysqli_real_escape_string($mysqli,$encrypted_pass)."', '".mysqli_real_escape_string($mysqli,$encrypted_hwid)."', '".mysqli_real_escape_string($mysqli,$try)."', '".mysqli_real_escape_string($mysqli,$active)."')";
 	$result = $mysqli->query($sql);
 
 	if (!$result) {
